8710afd5be864c0d820e58ddebeb209ded43dfc6
Clement Helsens <clementhelsens>
2025-04-29 09:07:24 +0200

    add sendjobs

--- README.md
+++ README.md
@@ -2,10 +2,20 @@
 
 ## Copy data to RCP cluster
 
-To copy data to the RCP cluster, need to have first copied the data on the NAS-RCP. A folder exist for that purpose 
+To copy data to the RCP cluster, need to have first copied the data on the NAS-RCP. Either the data is already in the raw data catalog and thus store on the NAS, or it needs to be copied. A folder exist for that purpose, it is located inside `common/ForRCP`. In this folder every user creates is own folder, for example `clement` and creates two sub-folders `input` and `output`. Resulting in something like `common/ForRCP/clement/input` and `common/ForRCP/clement/output`. In the input folder we will add all the data that we need to be copied to the cluster for processing, in the output folder, all the data produced during processing will be copied back (for example segmentation masks, model, text file). The cluster has a high performance scratch space of 2.5TB, but nothing is backup, and can be deleted when space is needed (for example when many TBs are sitting there for long time w/o accessing them). The only machine that has an access to both the lab NAS and the RCP scratch is the `JumpHost`. To connect to it, simply replace `<USERNAME>` with your gaspar and type in a terminal:
+
 ```bash
 ssh <USERNAME>@haas001.rcp.epfl.ch
 ```
+
+On this machine, our lab NAS is mounted as `/mnt/upoates/collaborative` and the scratch (accessible to the GPU cluster) as `/mnt/upoates/scratch`. If you want to list the files for RCP, do something like:
+
+```bash
+ls /mnt/upoates/collaborative/ForRCP
+```
+
+What we want at this point is to replicate all the files that we have inside `/mnt/upoates/collaborative/ForRCP` on the cluster's scratch `/mnt/upoates/scratch`. For that let's create the same structure. 
+
 
 id
 
@@ -41,7 +51,7 @@ docker login registry.rcp.epfl.ch
 docker push registry.rcp.epfl.ch/upoates-helsens/cellpose-env:latest
 docker push registry.rcp.epfl.ch/upoates-helsens/cellpose-env:v0.1
 
-get the file
+get kube config the file
 curl https://wiki.rcp.epfl.ch/public/files/kube-config.yaml -o ~/.kube/config && chmod 600 ~/.kube/config
 
 set the cluster
@@ -99,3 +109,6 @@ runai bash my-demo-job
   -- conda init \
   -- bash
 
+use this to login with different credentials
+  --run-as-uid UID \
+  --run-as-gid GID \
---
+++ docker_cellpose/sendjobs.py
@@ -0,0 +1,72 @@
+#python sendjobs.py --name cellpose-test-2 --image registry.rcp.epfl.ch/upoates-helsens/cellpose-env:v0.1 --gpu 1. --input /scratch/feyza/input/test2c --output /scratch/output/feyza --model /scratch/data/feyza/cellpose_training/2d/models/CP_20241007_h2bxncad -d 30 -c 2 1
+import argparse
+import os, sys
+import subprocess
+import glob
+from pathlib import Path
+
+#__________________________________________________________
+def getCommandOutput(command):
+    p = subprocess.Popen(command, shell = True, stdout = subprocess.PIPE, stderr = subprocess.PIPE,universal_newlines=True)
+    (stdout,stderr) = p.communicate()
+    return {"stdout":stdout, "stderr":stderr, "returncode":p.returncode}
+
+
+#__________________________________________________________
+def main():
+    parser = argparse.ArgumentParser(
+        description="Cellpose send jobs"
+    )
+
+    parser.add_argument("--name",   help="Name of the job")
+    parser.add_argument("--image",  help="Name of the image")
+    parser.add_argument("--gpu",  help="Number of GPUs", default=1, type=float)
+    parser.add_argument("--nas",    help="Path to the input files on the NAS", type=str)
+
+    parser.add_argument("--input",  help="Path to the input file (on the cluster)")
+    parser.add_argument("--output", help="Path to the output directory (on the cluster)")
+    parser.add_argument("--model",  help="Path to the model file (on the cluster)")
+    parser.add_argument("--diameter", "-d", default = 15, help="diameter for the model", type=float)
+    parser.add_argument("--anisotropy", "-a", default = 1.5, help="anisotropy", type=float)
+    parser.add_argument("--minsize", "-m", default = -1, help="minimum size", type=float)
+    
+    parser.add_argument("--channels", "-c", default = [0,0], help="channels for the model", type=int, nargs='+')
+    parser.add_argument("--test",  "-t", action="store_true",help="Run the first image as a test")
+    parser.add_argument("--verbose",  "-v", action="store_true",help="Increase output verbosity")
+    
+    args = parser.parse_args()
+    channels=" ".join(str(x) for x in args.channels)
+
+
+    nas_images = os.path.join(args.nas, "*.tif")
+    if not os.path.exists(args.nas):
+        print('Path to the input files on the NAS ===>{}<=== does not exist. Maybe UPOATES nas not mounted?'.format(args.nas))
+        sys.exit(3)
+
+    count=0
+    for img_file in glob.glob(nas_images):
+        if count>0 and args.test:break
+
+        img=img_file.split('/')[-1]
+        name = args.name + '-' + img.split('.')[0].replace('_','-').replace('--','-').replace(' ','-')
+        print('sending image = ',img, '  with job name  ', name)
+        cmd='runai submit --name {} --image {} --gpu {} --existing-pvc claimname=upoates-scratch,path=/scratch --command -- /usr/bin/python3 /home/helsens/3d_segmentation/3d_cellpose.py {} {} {} --diameter {} --channels {} --image {} --anisotropy {} --minsize {}'.format(name, args.image, args.gpu, args.input, args.output, args.model, args.diameter, channels, img, args.anisotropy, args.minsize)
+
+        print(cmd)
+        count+=1
+        outputCMD = getCommandOutput(cmd)
+        stderr=outputCMD["stderr"].split('\n')
+        stdout=outputCMD["stdout"].split('\n')
+
+        print('error:',stderr)
+        print('output'':',stdout)
+
+
+    #runai submit
+    #--name cellpose-test-2 
+    #--image registry.rcp.epfl.ch/upoates-helsens/cellpose-env:v0.1 --gpu 1. 
+    #--existing-pvc claimname=upoates-scratch,path=/scratch 
+    #--command -- /usr/bin/python3 /home/helsens/3d_segmentation/3d_cellpose.py /scratch/feyza/input/test2c /scratch/output/feyza /scratch/data/feyza/cellpose_training/2d/models/CP_20241007_h2bxncad -d 30 -c 2 1
+#/scratch/feyza/input/240925-pos4 /scratch/output/feyza /scratch/data/feyza/cellpose_training/2d/models/CP_20241007_h2bxncad
+
+main()
