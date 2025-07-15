8acdb1355e5f1a4ad92bd8d22af7994f5f51a633
Clement Helsens <clementhelsens>
2025-07-15 08:32:17 +0200

    add sam

--- docker_cellpose/sendjobs.py
+++ docker_cellpose/sendjobs.py
@@ -53,7 +53,7 @@ def main():
         safe_img_name = re.sub(r'[^a-z0-9]+', '-', img_base.lower()).strip('-')
         name = f"{args.name}-{safe_img_name}"
         print('sending image = ',img, '  with job name  ', name)
-        cmd='runai submit --name {} --image {} --gpu {} --existing-pvc claimname=upoates-scratch,path=/scratch --command -- /usr/bin/python3 /home/helsens/3d_segmentation/3d_cellpose.py {} {} {} --diameter {} --channels {} --image "{}" --anisotropy {} --minsize {}'.format(name, args.image, args.gpu, args.input, args.output, args.model, args.diameter, channels, img, args.anisotropy, args.minsize)
+        cmd='runai submit --name {} --image {} --gpu {} --existing-pvc claimname=upoates-scratch,path=/scratch --command -- /usr/bin/python3 /home/helsens/3d_segmentation/3d_cellpose_sam.py {} {} {} --diameter {} --channels {} --image "{}" --anisotropy {} --minsize {}'.format(name, args.image, args.gpu, args.input, args.output, args.model, args.diameter, channels, img, args.anisotropy, args.minsize)
 
         print(cmd)
         count+=1
